c9316703d1eadac98e574556fae7ebff58f42b58
Danial Shakeel <149463486+danial-23175@users.noreply.github.com>
2025-05-05 22:31:35 +0500

    Update Experience.js
    add my previous experience and also a current where now i am employee

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,4 +1,4 @@
-// src/pages/Experience.js
+
 import React from 'react';
 
 const Experience = () => {
@@ -6,7 +6,9 @@ const Experience = () => {
     <section id="experience" className="py-5 text-center bg-light">
       <div className="container">
         <h2>Experience</h2>
+    
         <p>2023 - Present: SEO and Digital marketing specialist .</p>
+    
         <p>2021 - 2023: Web developer in wordpress , shopify and digital marketing </p>
       </div>
     </section>
