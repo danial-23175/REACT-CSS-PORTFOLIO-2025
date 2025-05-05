12fa65353239863b39c6ef832a2e56c07e2bfbe1
Danial Shakeel <149463486+danial-23175@users.noreply.github.com>
2025-05-05 22:19:35 +0500

    adding the main line in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,13 @@
-// src/components/Footer.js
+
 import React from 'react';
 
 const Footer = () => {
+  
   return (
     <footer className="bg-dark text-white text-center py-4">
+    
       <p>&copy; 2025 Danial shakeel portfolio. All rights reserved.</p>
+    
     </footer>
   );
 };
