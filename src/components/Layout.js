25b606f2a96419080fed030ec16118ead6f4dad4
Danial Shakeel <149463486+danial-23175@users.noreply.github.com>
2025-05-05 22:22:26 +0500

    add code to import and pages

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,10 @@
-// src/components/Layout.js
+
 import React from 'react';
 import Sidebar from './Sidebar';
 import Header from './Header';
 import Footer from './Footer';
 
-// Import all sections directly (as components)
+
 import Home from '../pages/Home';
 import About from '../pages/About';
 import Skills from '../pages/Skills';
@@ -13,7 +13,9 @@ import Experience from '../pages/Experience';
 import Projects from '../pages/Projects';
 import Contact from '../pages/Contact';
 
+
 const Layout = () => {
+  
   return (
     <div className="d-flex" id="wrapper">
       <Sidebar />
